02789362a6fe942f4849b793a13a2e4d095c6623
Srinath V Divate <srinathdivate123@gmail.com>
2022-09-23 18:35:41 +0530

    4

--- expenseswebsite/settings.py
+++ expenseswebsite/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [ 'sriexpenses.herokuapp.com' ,'127.0.0.1', 'git.heroku.com']
+ALLOWED_HOSTS = [ 'sriexpenses.herokuapp.com' ,'127.0.0.1', 'git.heroku.com/sriexpenses.git']
 
 
 # Application definition
